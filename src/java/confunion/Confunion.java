471ae83f76f96f49f65b51529dda1ea89cb74df8
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2017-11-06 15:29:21 +0100

    Added new top-level function to Java API and rewrote comments in EN

--- src/java/confunion/Confunion.java
+++ src/java/confunion/Confunion.java
@@ -2,60 +2,70 @@ package confunion;
 
 import clojure.java.api.Clojure;
 import clojure.lang.IFn;
-
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
 /**
- * Facade per accedere alle funzioni offerte dalla libreria tramite Java.
+ * Façade to access Confunion functions through Java.
  */
 @SuppressWarnings("unused")
 public class Confunion {
-
-  private final IFn applyFn;
-  private final IFn confunionFn;
   private final IFn addPropertiesFn;
   private final IFn loadConfigurationFn;
+  private final IFn composeConfiguration;
 
   public Confunion() {
     IFn require = Clojure.var("clojure.core", "require");
 
-    applyFn = Clojure.var("clojure.core", "apply");
-
     require.invoke(Clojure.read("confunion.core"));
-    confunionFn = Clojure.var("confunion.core", "confunion");
     loadConfigurationFn = Clojure.var("confunion.core", "load-configuration");
+    composeConfiguration = Clojure.var("confunion.core", "compose-configuration");
 
     require.invoke(Clojure.read("confunion.properties"));
     addPropertiesFn = Clojure.var("confunion.properties", "add-properties");
   }
 
   /**
-   * Costruisce una mappa di configurazione ottenuta effettuando il merge del file EDN di base
-   * con il contenuto del primo esistente nella lista di overrides.
+   * Builds a configuration map by merging the contents of the base EDN file content with the first existing override
+   * one.
    *
-   * @param schemaPath    Path del file EDN contenente lo schema che descrive la struttura che la configurazione
-   *                      finale dovrà rispettare
-   * @param basePaths     Lista *ordinata* di path dei possibili file EDN con la configurazione di base
-   *                      (NB: almeno uno deve esistere)
-   * @param overridePaths Lista *ordinata* di path dei possibili file EDN con la seconda mappa da unire alla prima
-   * @return Unione della mappa di configurazione di base con eventualmente un'altra con gli
-   * <em>overrides</em>
+   * @param schemaPath Path of the EDN file that describes the structure of the final configuration.
+   * @param basePaths Ordered paths list of the possible base EDN configurations files (NB: at least one must exists).
+   * @param overridePaths Ordered paths list of possible overrides EDN file, that will be merged with the base one.
+   * @return Validated configuration (obtained by merging the base with the override one) or an exception
    */
   public Map loadConfiguration(String schemaPath, List<String> basePaths, List<String> overridePaths) {
     return (Map) loadConfigurationFn.invoke(schemaPath, basePaths, overridePaths);
   }
 
   /**
-   * Aggiunge proprietà all'oggetto {@link java.util.Properties} specificato a partire
-   * dalla mappa di configurazione passata (eventualmente sovrascrivendo quelle preesistenti).
+   * Populate {@link Properties} using a configuration map.
+   * <p>
+   * If a property specified in the configuration already exists in the input {@link Properties}, it'll be overwritten.
+   * </p>
    *
-   * @param p Oggetto {@link java.util.Properties} nel quale inserire le proprietà
-   * @param m Mappa con le proprietà da inserire, può essere una mappa in formato EDN e
-   *          sia le chiavi che i valori verranno serializzati in stringhe.
+   * @param p {@link java.util.Properties} object to populate
+   * @param m Configuration map that will be "serialized" in the input {@link Properties} object. Keywords and values
+   *          will be serialized in plain strings.
    */
   public void addProperties(Properties p, Map m) {
     addPropertiesFn.invoke(p, m);
+  }
+
+  /**
+   * Builds a configuration map by merging the contents of the base EDN file content with the first existing override
+   * one.
+   *
+   * @param schemaPath Ordered list of paths to look for the base schema EDN file.
+   * @param additionalSchemaPath Ordered list of paths look for the (optional) additional schema EDN file to merge with
+   *          the base one.
+   * @param basePaths Ordered paths list of the possible base EDN configurations files (NB: at least one must exists).
+   * @param overridePaths Ordered paths list of possible overrides EDN file, that will be merged with the base one.
+   * @return Validated configuration (obtained by merging the base with the override one) or an exception
+   */
+  public Map composeConfiguration(List<String> schemaPath, List<String> additionalSchemaPath, List<String> basePaths,
+      List<String> overridePaths) {
+    return (Map) composeConfiguration.invoke(schemaPath, additionalSchemaPath, basePaths, overridePaths);
   }
 }
--- src/java/confunion/ConfunionEngine.java
+++ src/java/confunion/ConfunionEngine.java
@@ -2,7 +2,6 @@ package confunion;
 
 import clojure.java.api.Clojure;
 import clojure.lang.IFn;
-
 import java.util.Map;
 
 /**
@@ -34,45 +33,43 @@ public class ConfunionEngine {
   }
 
   /**
-   * Costruisce una mappa di configurazione facendo il merge del contenuto delle
-   * mappe EDN contenute nei file ai percorsi indicati, dal primo (base) all'ultimo).
+   * Builds a configuration map by merging multiple EDN map files in the given paths, loaded from first to last.
    *
-   * @param paths Numero variabile di path dei file EDN da cui leggere le mappe di configurazione
-   * @return Mappa unione delle strutture dati contenute ai file indicati
+   * @param paths Paths of the files that contain EDN configuration maps.
+   * @return Merged configuration map
    */
   public Map confunion(String... paths) {
     return (Map) applyFn.invoke(confunionFn, paths);
   }
 
   /**
-   * Legge un file in formato EDN.
+   * Read an EDN file.
    *
-   * @param path Path del file EDN da leggere
-   * @return Oggetto che rappresenta la struttura dati top-level definita
-   * nel file EDN (lista, mappa, set, ecc)
+   * @param path Path of the EDN file
+   * @return Object that represents the top-level data structure defined in the input EDN file (list, map, set, etc)
    */
   public Object loadEdn(String path) {
     return loadEdnFn.invoke(path);
   }
 
   /**
-   * Valida uno schema per controllare se è ben formato.
+   * Validate a schema for well-formedness.
    *
-   * @param schema Mappa contenente la definizione dei parametri di configurazione
-   * @return Schema se ben formato, altrimenti genera un'eccezione con un messaggio
-   * di errore dettagliato con tutti gli errori di validazione riscontrati.
+   * @param schema Configuration specification
+   * @return The input schema if well-formed according to the specification, otherwise throws an exception with a
+   *         detailed error message explaining the validation errors.
    */
   public Map validateSchema(Map schema) {
     return (Map) verifySchemaFn.invoke(schema);
   }
 
   /**
-   * Valida una configurazione usando uno schema.
+   * Validate a configuration against a schema.
    *
-   * @param configuration Mappa contenente la configurazione
-   * @param schema        Mappa contenente la definizione dei parametri di configurazione
-   * @return La configurazione stessa se corretta, altrimenti viene lanciata un'eccezione con
-   * un messaggio che dettagli quali sono gli errori di validazione riscontrati
+   * @param configuration Configuration map
+   * @param schema Schema (configuration specification)
+   * @return The input configuration if well-formed according the given schema, otherwise throwns an exception with a
+   *         detailed error message explaining the validation errors.
    */
   public Map validateConfiguration(Map configuration, Map schema) {
     return (Map) verifyConfFn.invoke(configuration, schema);
